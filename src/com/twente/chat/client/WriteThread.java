1e1a8e025de0c2f0dad5400dba1c1b245cdb7447
”Faisal Afzal” <faisal@info.nl>
2018-01-24 14:23:57 +0100

    Sample chat application

---
+++ src/com/twente/chat/client/WriteThread.java
@@ -0,0 +1,54 @@
+package com.twente.chat.client;
+
+import java.io.IOException;
+import java.io.OutputStream;
+import java.io.PrintWriter;
+import java.net.Socket;
+import java.util.Scanner;
+
+public class WriteThread extends Thread {
+    private PrintWriter writer;
+    private Socket socket;
+    private ChatClient client;
+
+    public WriteThread(Socket socket, ChatClient client) {
+        this.socket = socket;
+        this.client = client;
+
+        try {
+            OutputStream output = socket.getOutputStream();
+            writer = new PrintWriter(output, true);
+        } catch (IOException ex) {
+            System.out.println("Error getting output stream: " + ex.getMessage());
+            ex.printStackTrace();
+        }
+    }
+
+    public void run() {
+
+        Scanner scanner = new Scanner(System.in);
+
+        System.out.println("Enter your name: ");
+
+        String userName = scanner.nextLine();
+
+        client.setUserName(userName);
+        writer.println(userName);
+
+        String text;
+
+        do {
+            System.out.println("[" + userName + "]: ");
+            text = scanner.nextLine();
+            writer.println(text);
+
+        } while (!text.equals("bye"));
+
+        try {
+            socket.close();
+        } catch (IOException ex) {
+
+            System.out.println("Error writing to server: " + ex.getMessage());
+        }
+    }
+}
